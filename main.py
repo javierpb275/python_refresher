f9d521374dcdb0a9d421da803e8f636567a86b36
javierpb275 <javierpb275@gmail.com>
2022-05-16 19:33:00 +0200

    variables

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-if __name__ == '__main__':
-    print('Hello World!')
 
+if __name__ == '__main__':
+    pass
---
+++ variables.py
@@ -0,0 +1,23 @@
+x = 15
+price = 9.99
+discount = 0.2
+name = "Rolf"
+name2 = 'pepe'
+
+a = 25
+b = a
+
+result = price * (1 - discount)
+
+print(result)
+print(name)
+print(name + name2)
+print(name * 2)
+
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
