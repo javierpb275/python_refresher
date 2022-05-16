997493bc7658f3deac10c49bd95b1a0fa264562b
javierpb275 <javierpb275@gmail.com>
2022-05-16 21:20:51 +0200

    booleans

---
+++ 06_booleans/booleans.py
@@ -0,0 +1,15 @@
+truth = True
+lie = False
+print(5 == 5)
+print(5 > 5)
+print(5 != 5)
+print(5 <= 5)
+print(5 >= 5)
+
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+'''True'''
+print(friends == abroad)
+'''False'''
+print(friends is abroad)
