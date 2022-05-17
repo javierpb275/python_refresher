31377a2665d0c9b9eea80a5f022a3497d3b6595a
javierpb275 <javierpb275@gmail.com>
2022-05-17 13:16:52 +0200

    in keyword

---
+++ 08_in_keyword/in_keyword.py
@@ -0,0 +1,6 @@
+friends = ["Rolf", "Bob", "Jen"]
+print("Jen" in friends)
+
+movies_watched = {"The Matrix", "Green Book", "Her"}
+user_movie = input("Enter something you've watched recently: ")
+print(user_movie in movies_watched)
