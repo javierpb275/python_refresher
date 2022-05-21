cbb83f0939f78b302db1be15eed75bd69d20390c
javierpb275 <javierpb275@gmail.com>
2022-05-21 17:20:31 +0200

    dunder methods: str and repr

---
+++ 15_str_and_repr/str_and_repr.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}"
+
+    def __repr__(self):
+        return f"<Person('{self.name}', {self.age})>"
+
+
+bob = Person("Bob", 25)
+print(bob)  # <__main__.Person object at 0x000001992337CEE0> (without __str__(self))
+print(bob)  # Name: Bob, Age: 25 (with __str__(self))
+print(bob.__repr__())  # <Person('Bob', 25)>
+print(repr(bob))  # <Person('Bob', 25)>
