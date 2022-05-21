af3ca3bd90f7face9305e36d834a54698f86c381
javierpb275 <javierpb275@gmail.com>
2022-05-21 19:48:04 +0200

    type hinting

---
+++ 20_type_hinting/type_hinting.py
@@ -0,0 +1,36 @@
+from typing import List  # List, Tuple, Set, etc...
+
+
+def list_avg(sequence: List) -> float:
+    return sum(sequence) / len(sequence)
+
+
+print(list_avg([1, 2, 3]))
+
+
+class Book:
+    TYPES = ("hardcover", "paperback")
+
+    def __init__(self, name: str, book_type: str, weight: int):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr__(self) -> str:
+        return f"<Book('{self.name}', '{self.book_type}', {self.weight})>"
+
+    @classmethod
+    def hardcover(cls, name: str, page_weight: int) -> "Book":
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    @classmethod
+    def paperback(cls, name: str, page_weight: int) -> "Book":
+        return cls(name, cls.TYPES[1], page_weight)
+
+
+class BookShelf:
+    def __init__(self, books: List[Book]):
+        self.books = books
+
+    def __str__(self) -> str:
+        return f"BookShelf with {len(self.books)} books."
