ea527ffdcbd61c30a2cf0f41d677c02ce5fea437
javierpb275 <javierpb275@gmail.com>
2022-05-22 18:35:44 +0200

    decorators with parameters

---
+++ 27_decorators_with_parameters/decorators_with_parameters.py
@@ -0,0 +1,36 @@
+import functools
+
+user = {"username": "pepe", "access_level": "user"}
+
+
+def make_secure(access_level):
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["access_level"] == access_level:
+                return func(*args, **kwargs)
+            else:
+                return f"No {access_level} permissions for {user['username']}."
+
+        return secure_function
+
+    return decorator
+
+
+@make_secure("admin")
+def get_admin_password():
+    return "admin: 1234"
+
+
+@make_secure("user")
+def get_dashboard_password():
+    return "user: user_password"
+
+
+print(get_admin_password())
+print(get_dashboard_password())
+
+user = {"username": "pepe", "access_level": "admin"}
+
+print(get_admin_password())
+print(get_dashboard_password())
