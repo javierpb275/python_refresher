92140030172d7301e5403b8e265b424c6399dfaa
javierpb275 <javierpb275@gmail.com>
2022-05-22 17:54:27 +0200

    @ syntax for decorators

--- 26_decorators/decorators.py
+++ 26_decorators/decorators.py
@@ -1,18 +1,23 @@
+import functools
+
 user = {"username": "pepe", "access_level": "guest"}
 
 
-def get_admin_password():
-    return '1234'
-
-
 def make_secure(func):
+    @functools.wraps(func)  # keeps the name and docs of get_admin_password function
     def secure_function():
         if user["access_level"] == "admin":
             return func()
         else:
             return f"No admin permissions for {user['username']}."
+
     return secure_function
 
 
-get_admin_password = make_secure(get_admin_password)
+@make_secure
+def get_admin_password():
+    return '1234'
+
+
 print(get_admin_password())
+print(get_admin_password.__name__)
