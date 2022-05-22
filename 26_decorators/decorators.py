2f0e9790b1f62b4d2118d2bb6f3942465c7378c8
javierpb275 <javierpb275@gmail.com>
2022-05-22 18:07:49 +0200

    decorating functions with parameters

--- 26_decorators/decorators.py
+++ 26_decorators/decorators.py
@@ -5,9 +5,9 @@ user = {"username": "pepe", "access_level": "guest"}
 
 def make_secure(func):
     @functools.wraps(func)  # keeps the name and docs of get_admin_password function
-    def secure_function():
+    def secure_function(*args, **kwargs):
         if user["access_level"] == "admin":
-            return func()
+            return func(*args, **kwargs)
         else:
             return f"No admin permissions for {user['username']}."
 
@@ -15,9 +15,12 @@ def make_secure(func):
 
 
 @make_secure
-def get_admin_password():
-    return '1234'
+def get_password(panel):
+    if panel == "admin":
+        return '1234'
+    elif panel == "billing":
+        return 'password'
 
 
-print(get_admin_password())
-print(get_admin_password.__name__)
+print(get_password("billing"))
+print(get_password.__name__)
